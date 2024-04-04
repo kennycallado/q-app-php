1c986832edf77b0a234c00dbdeda2ff12a794333
kennycallado <kennycallado@hotmail.com>
2024-04-04 17:32:28 +0200

    ADD basic join model

---
+++ src/App/Models/Join.php
@@ -0,0 +1,24 @@
+<?php
+namespace Src\App\Models;
+
+class Join
+{
+    private string $id;
+    private string $in; // user id
+    private string $out; // project id 
+    private bool $completed;
+    private ?object $score;
+
+    public function __construct(string $id, string $in, string $out, bool $completed = false)
+    {
+        $this->id = $id;
+        $this->in = $in;
+        $this->out = $out;
+        $this->completed = $completed;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+}
