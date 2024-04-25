a60f38ba2f2f630ae9e91edd8d5bf077e6ce7c6b
kennycallado <kennycallado@hotmail.com>
2024-04-25 10:23:09 +0200

    ADD initial setup for devcontainer

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+mkdir -p "${HOME}/.local/bin"
+mkdir -p "${HOME}/.config/lvim"
+PATH="${PATH}:${HOME}/.local/bin"
+
+bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
+# LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) --no-install-dependencies -y
+
+ln -sf $PWD/.devcontainer/lvim-config.lua $HOME/.config/lvim/config.lua && set +x
+
+exit 0
