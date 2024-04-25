d67e1493e640d25a92d2604a0bd4be4756f748f3
kennycallado <kennycallado@hotmail.com>
2024-04-25 10:53:47 +0200

    UPDATE

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,10 +4,23 @@ mkdir -p "${HOME}/.local/bin"
 mkdir -p "${HOME}/.config/lvim"
 PATH="${PATH}:${HOME}/.local/bin"
 
+echo "Installing needed tools"
+
+# ripgrep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_14.0.0_amd64.deb
+dpkg -i ripgrep_14.0.0_amd64.deb
+
+# surrealdb cli
+curl --proto '=https' --tlsv1.2 -sSf https://install.surrealdb.com | sh
+
 echo "Installing LunarVim"
-# bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
-# LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) --no-install-dependencies -y
+# neovim
+bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
 
-# ln -sf $PWD/.devcontainer/lvim-config.lua $HOME/.config/lvim/config.lua && set +x
+# lunarvim
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) --no-install-dependencies -y
 
-# exit 0
+# lvim config
+ln -sf $PWD/.devcontainer/lvim-config.lua $HOME/.config/lvim/config.lua && set +x
+
+exit 0
