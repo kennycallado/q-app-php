efdfc62bed628e6272de0ad29fdba94ade077ac9
kennycallado <kennycallado@hotmail.com>
2024-04-26 20:02:04 +0200

    UPDATE

--- flake.nix
+++ flake.nix
@@ -7,32 +7,36 @@
     flake-compat.url = "github:nix-community/flake-compat";
   };
 
-  outputs = { nixpkgs , systems, ... }:
-  let
-    eachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
+  outputs = { nixpkgs, systems, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
 
-    devShells = eachSystem (system: {
-      default =
-      let
-        pkgs = import nixpkgs { inherit system; };
-        # just to make it easier
-        altEditor = true;
-        extra = true;
-      in pkgs.mkShell {
-        packages = with pkgs; [
-          php82
-          php82Packages.composer
-        ]
-        ++ (if altEditor then [pkgs.neovim pkgs.lunarvim] else [])
-        ++ (if extra then [pkgs.nodejs_18] else []);
+      devShells = eachSystem (system: {
+        default =
+          let
+            pkgs = import nixpkgs { inherit system; };
+            # just to make it easier
+            altEditor = true;
+            extra = true;
+          in
+          pkgs.mkShell {
+            packages = with pkgs; [
+              php82
+              php82Packages.composer
+            ]
+            ++ (if altEditor then [ pkgs.neovim pkgs.lunarvim ] else [ ])
+            ++ (if extra then [ pkgs.nodejs_18 ] else [ ]);
 
-        shellHook = ''
-          PATH="$PATH:$HOME/.local/bin"
+            shellHook = ''
+              PATH="$PATH:$HOME/.local/bin"
 
-          echo "ready to rock! 🚀"
-        '';
-      };
-    });
-  };
+              echo "ready to rock! 🚀"
+            '';
+          };
+      });
+
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    };
 }
