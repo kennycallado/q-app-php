a00043476c33438007b01b163f5ebbcd5f5b597b
kennycallado <kennycallado@hotmail.com>
2024-05-05 18:04:07 +0200

    FIX dev: there was not early return when request for statics

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ use Src\Core\RouteCollection;
 if (!isset($_ENV['ENVIRONMENT']) || strtolower($_ENV['ENVIRONMENT']) !== 'production') {
     if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
         handle_statics($_SERVER["REQUEST_URI"]);
+        return;
     }
 
     if (file_exists(__DIR__ . '/../.env')) {
@@ -41,28 +42,29 @@ function handle_statics($temp_uri) {
     if (preg_match('/^\/public\/assets\/bootstrap\/(.+)$/', $temp_uri)) {
         $temp_path = str_replace("/public/assets/bootstrap/", "", $temp_uri);
 
-        switch (true) {
-            case preg_match('/\.(?:css)$/', $temp_uri):
-                header('Content-type: text/css');
-                break;
-
-            case preg_match('/\.(?:js)$/', $temp_uri):
-                header('Content-Type: text/javascript');
-                break;
-
-            default:
-                break;
-        }
+        set_headers($temp_uri);
     
         readfile(__DIR__ . '/../vendor/twbs/bootstrap/' . $temp_path);
         return;
     } else {
-    // } elseif (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $temp_uri)) {
-        if (preg_match('/\.(?:css)$/', $temp_uri)) {
-            header('Content-type: text/css');
-        }
+        set_headers($temp_uri);
 
         readfile(__DIR__ . "/..$temp_uri");
         return;
+    }
+}
+
+function set_headers($temp_uri) {
+    switch (true) {
+        case preg_match('/\.(?:css)$/', $temp_uri):
+            header('Content-type: text/css');
+            break;
+
+        case preg_match('/\.(?:js)$/', $temp_uri):
+            header('Content-Type: text/javascript');
+            break;
+
+        default:
+            break;
     }
 }
