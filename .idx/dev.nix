33bf93d8fcfb4dee957ce7f29c20eae78a7b8b83
kennycallado <kennycallado@gmail.com>
2024-05-17 20:55:58 +0000

    initial config file for idx

---
+++ .idx/dev.nix
@@ -0,0 +1,65 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = with pkgs; [
+    php
+    php82Packages.composer
+    podman
+    podman-compose
+    docker
+    docker-compose
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "bmewburn.vscode-intelephense-client"
+      "arrterian.nix-env-selector"
+      "surrealdb.surrealql"
+      "jnoortheen.nix-ide"
+      "humao.rest-client"
+      "lkrms.pretty-php"
+      "vscodevim.vim"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["php" "-S" "0.0.0.0:$PORT" "public/index.php"];
+          manager = "web";
+          env = {
+            # Environment variables to set for your server
+            PORT = "$PORT";
+          };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        composer-install ="composer install";
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        init-db = "surreal start -A memory";
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
